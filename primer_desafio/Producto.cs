d3ab050a63595c6c829f8d64723d1db090da3450
Antonio Campusano Paulino <100012466+31Antonio@users.noreply.github.com>
2022-11-07 23:34:36 -0400

    Agregar archivos de proyecto.

---
+++ primer_desafio/Producto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primer_desafio_de_la_clase
+{
+    public class Producto
+    {
+        public int id { get; set; }
+        public string descripcion { get; set; }
+        private float precioVenta { get; set; }
+        private int stock { get; set; }
+        private int idUsuario { get; set; }
+
+
+        public Producto(int id, string descripcion, float precioVenta, int stock, int idUsuario)
+        {
+            this.id = id;
+            this.descripcion = descripcion;
+            this.precioVenta = precioVenta;
+            this.stock = stock;
+            this.idUsuario = idUsuario;
+        }
+    }
+}
